ad3c09d87a841fbcac5acc77cc2a1003547c3b6f
KCEvers <kyra.c.evers@gmail.com>
2023-08-28 09:46:03 +0200

    update

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,13 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' hello
+#'
+#' @param name A name
+#'
+#' @return A message
+#' @export
+#'
+#' @examples hello("Alice")
+hello <- function(name){
+  message <- paste0("Hello ", name, "!")
 
-hello <- function() {
-  print("Hello, world!")
+  return(message)
 }
